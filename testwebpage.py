6380af65b19d0afe4313f91b7f612b4c5ddf7d6b
Dev Bahadur Poudel <d.poudel@inventronicsglobal.com>
2026-04-15 14:14:31 +0200

    new test added

--- testwebpage.py
+++ testwebpage.py
@@ -3,19 +3,20 @@ import re
 from playwright.sync_api import Page,expect
 import time
 import pytest
-
-@pytest.fixture(scope="function", autouse=True)
-def before_each_after_each(page: Page):
-    
+ 
+@pytest.fixture(scope="function",autouse=True)
+def landpage(page:Page):
+            
     print("before the test runs")
 
     # Go to the starting url before each test.
     page.goto("https://devpoudel.onrender.com/")
-    yield
+    yield 
     
     print("after the test runs")
 
 class TestLandingPage:    
+   
 
     def test_has_title(self, page:Page):
       
@@ -29,4 +30,21 @@ class TestLandingPage:
       expect(page.locator("h2.hero-title")).to_have_text(
         "Software Engineer · Data Analyst · AI/ML Practitioner")
 
+    def test_click_contact(self, page:Page):
 
+       contact=page.get_by_role("link",name="Contact").click()
+       page.get_by_role("textbox",name="name").fill("Peter")
+       page.get_by_placeholder("your@email.com").fill("toyou.dev@gmail.com")
+       page.get_by_role("textbox",name="message").fill("hello this is a test messgae")
+       page.get_by_role("button",name="Send Message").click()
+    
+    def test_all_Locators(self,page:Page):
+       for loc in page.get_by_role("link").all():
+           loc.click()
+           #locator.all().
+
+    #def test_menu(self,page:Page):
+    #   print(page.get_by_role('link').all())
+    #   for li in  page.get_by_role('link').all():
+    #     li.click();
+    #     time.sleep(2)
